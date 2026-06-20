bf4bb2a33c468a1824cbe3eb65fb026606fb2767
furatfatima11-boop <furat.fatima11@gmail.com>
2026-06-20 10:43:26 -0500

    Add files via upload

---
+++ GradeBook.py
@@ -0,0 +1,8 @@
+dictionary1 = {"name":"sarah","score":90}
+dictionary2 ={"name":"alex","score":70}
+dictionary3 = {"name":"jennie","score":80}
+dictionary4 = {"name":"evan","score:":100}
+dictionary5 = {"name":"kelly","score":60}
+for i in range:
+    average = 
+student = input('What is the name of the student you are looking for:')
